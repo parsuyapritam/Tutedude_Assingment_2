e9ed585faa130682b4583e898b2179594c76c2e8
parsuyapritam <parsuyapritam@gmail.com>
2025-04-30 16:21:08 +0530

    Add files via upload

---
+++ Tutedude_Assingment_2.py
@@ -0,0 +1,17 @@
+#task 1
+
+a=int(input('Enter a number: '))
+if a % 2 == 0:
+    print(a,'is an even number\n')
+else:
+    print(a,'is an old number\n')
+
+#task 2
+b=0
+for i in range(1,a+1):
+   b= b + i
+print("The sum of numbers from 1 to",a,"is:",b)
+
+#end
+
+
